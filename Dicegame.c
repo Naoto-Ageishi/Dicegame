9b8561d3bd7c41f9758623003c9ae4a02e8e70db
Naoto-Ageishi <40936016+Naoto-Ageishi@users.noreply.github.com>
2018-07-12 13:18:47 +0900

    Add files via upload

---
+++ Dicegame.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int d1,d2,total;
+  char name[50];
+
+  printf("What is your name?\n");
+  printf("> ");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+  
+  printf("Rolling the dice...\n");
+  srand(time(NULL));
+  d1 = rand() % 6 + 1;
+  d2 = rand() % 6 + 1;
+  total = d1 + d2;
+  printf("Die 1: %d\n",d1);
+  printf("Die 2: %d\n",d2);
+  printf("Total value: %d\n",total);
+
+  if(total >= 7){
+    printf("%s win!\n",name);
+  }else printf("%s lost\n",name);
+  
+  return 0;
+}
